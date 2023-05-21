92811a4f925c21fcc83dac11897cab104ea0b278
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2023-05-21 15:54:36 +0200

    I had apparantly forgotten to add a svelte:head to blog and covers lol

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,6 +6,11 @@
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<title>Blog | Tristan Lukens</title>
+	<meta name="description" content="My blog. Read some posts if you want!" />
+</svelte:head>
+
 <ul class="space-y-4">
 	{#each data.posts as post}
 		<li>
--- src/routes/covers/+page.svelte
+++ src/routes/covers/+page.svelte
@@ -6,6 +6,14 @@
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<title>Covers | Tristan Lukens</title>
+	<meta
+		name="description"
+		content="All my drum covers, along with my bass covers. Watch one or two if you want!"
+	/>
+</svelte:head>
+
 <ul class="space-y-4">
 	{#each data.covers as cover}
 		<li>

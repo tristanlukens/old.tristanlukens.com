d6516116bf0c299f4b313c0f237277c130b40cd8
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2023-07-16 13:33:29 +0200

    made progress

--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -1,27 +1,38 @@
 <script lang="ts">
 	import type { post } from '$lib/server/database';
 
+	import { filters } from '$lib/stores';
+
 	import Tag from './tag.svelte';
 
 	export let post: post;
 </script>
 
-<a href="/blog/post/{post.slug}">
-	<div
-		class="flex flex-col gap-2 sm:gap-0 sm:flex-row justify-between bg-neutral-50 py-6 px-4 text-left hover:border-amber-400 hover:cursor-pointer border-l-4 border-amber-500 rounded transition space-y-2"
-	>
-		<p class="flex items-center sm:max-w-[65%] sm:min-w-[50%]">{post.title}</p>
-		<div class="flex flex-col space-y-2">
-			<p class="text-xs italic sm:text-right line-clamp-1">
-				written on {post.uploadDate}
-			</p>
-			<ul class="justify-end flex-wrap gap-1 hidden sm:flex">
-				{#each post.tags as tag}
-					<li>
-						<Tag xsText={true} {tag} />
-					</li>
-				{/each}
-			</ul>
+<form action={`/blog/post/${post.slug}`}>
+	<button class="w-full">
+		<div
+			class="flex flex-col gap-2 sm:gap-0 sm:flex-row justify-between bg-neutral-50 py-6 px-4 text-left hover:border-amber-400 hover:cursor-pointer border-l-4 border-amber-500 rounded transition space-y-2"
+		>
+			<p class="flex items-center sm:max-w-[65%] sm:min-w-[50%]">{post.title}</p>
+			<div class="flex flex-col space-y-2">
+				<p class="text-xs italic sm:text-right line-clamp-1">
+					written on {post.uploadDate}
+				</p>
+				<ul class="justify-end flex-wrap gap-1 hidden sm:flex">
+					{#each post.tags as tag}
+						<li>
+							<button
+								on:click|preventDefault={() => {
+									if (!$filters.includes(tag)) $filters.push(tag);
+									$filters = $filters;
+								}}
+							>
+								<Tag xsText={true} {tag} />
+							</button>
+						</li>
+					{/each}
+				</ul>
+			</div>
 		</div>
-	</div>
-</a>
+	</button>
+</form>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import type { post } from '$lib/server/database';
+
+	import { filters } from '$lib/stores';
 
 	import Post from '$lib/components/post.svelte';
+	import Tag from '$lib/components/tag.svelte';
 
 	export let data: PageData;
+
+	// this does not work yet...
+	// O(n^3) lol
+	let filteredPosts: post[] = [];
+	filteredPosts = data.posts;
+	$: filteredPosts.forEach((post) => {
+		post.tags.forEach((tag) => {
+			$filters.forEach((filter) => {
+				if (filter !== tag) {
+					const index = filteredPosts.indexOf(post);
+					filteredPosts.splice(index, 1);
+				}
+			});
+		});
+	});
+	$filters = $filters;
 </script>
 
 <svelte:head>
@@ -11,8 +31,25 @@
 	<meta name="description" content="My blog. Read some posts if you want!" />
 </svelte:head>
 
+<p class="italic text-center">Click tags to add filters. Applied filters:</p>
+<ul class="flex flex-wrap gap-1 mb-8 mt-2 justify-center">
+	{#each $filters as tag}
+		<li>
+			<button
+				on:click={() => {
+					const index = $filters.indexOf(tag);
+					$filters.splice(index, 1);
+					$filters = $filters;
+				}}
+			>
+				<Tag {tag} />
+			</button>
+		</li>
+	{/each}
+</ul>
+
 <ul class="space-y-4">
-	{#each data.posts as post}
+	{#each filteredPosts as post}
 		<li>
 			<Post {post} />
 		</li>
